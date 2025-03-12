a9636ff3ae4ec5ed90a2560c71f1c662e795e412
afdhali <afdhali.apreto@gmail.com>
2025-03-12 15:39:31 +0700

    basics -2

--- src/app/feed/layout.tsx
+++ src/app/feed/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <div>
       <div className="p-4 bg-rose-500 w-full">Navbar</div>
