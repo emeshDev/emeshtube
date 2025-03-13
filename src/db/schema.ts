0511d901905350b59f8d3b331684c33fbd051d11
afdhali <afdhali.apreto@gmail.com>
2025-03-13 22:06:12 +0700

    clerk webhook

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -11,10 +11,11 @@ export const users = pgTable(
   {
     id: uuid("id").primaryKey().defaultRandom(),
     clerkId: text("clerk_id").unique().notNull(),
+    name: text("name").notNull(),
     //   TODO: add banner fields
     imageUrl: text("image_url").notNull(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
   },
   (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
 );
