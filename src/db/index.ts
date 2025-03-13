657e4b30e3cdbca38506f266bc3c8e19c988f94b
afdhali <afdhali.apreto@gmail.com>
2025-03-13 15:57:18 +0700

    drizzle config & setup

---
+++ src/db/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle({ client: sql });
