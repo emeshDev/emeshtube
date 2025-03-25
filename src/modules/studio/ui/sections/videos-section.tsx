480882d11a3a075771df62e18fbf206fec10795a
afdhali <afdhali.apreto@gmail.com>
2025-03-25 13:41:47 +0700

    videos studio table

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import Link from "next/link";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -21,7 +30,7 @@ export const VideosSection = ({ categoryId }: Props) => {
 };
 
 const VideosSectionSuspense = ({ categoryId }: Props) => {
-  const [data, query] = trpc.studio.infiniteVideos.useSuspenseInfiniteQuery(
+  const [videos, query] = trpc.studio.infiniteVideos.useSuspenseInfiniteQuery(
     {
       limit: DEFAULT_LIMIT,
       categoryId,
@@ -32,14 +41,48 @@ const VideosSectionSuspense = ({ categoryId }: Props) => {
   );
 
   return (
-    <>
-      {JSON.stringify(data)}
+    <div>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <Link
+                  href={`/studio/videos/${video.id}`}
+                  key={video.id}
+                  legacyBehavior
+                >
+                  <TableRow className="cursor-pointer">
+                    <TableCell>{video.title}</TableCell>
+                    <TableCell>Visibility</TableCell>
+                    <TableCell>status</TableCell>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Comments</TableCell>
+                    <TableCell>Likes</TableCell>
+                  </TableRow>
+                </Link>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
       <InfiniteScroll
         isManual
         hasNextPage={query.hasNextPage}
         isFetchingNextPage={query.isFetchingNextPage}
         fetchNextPage={query.fetchNextPage}
       />
-    </>
+    </div>
   );
 };
--- src/modules/studio/ui/view/studio-view.tsx
+++ src/modules/studio/ui/view/studio-view.tsx
@@ -7,7 +7,15 @@ type Props = {
 export const StudioView = ({ categoryId }: Props) => {
   return (
     <>
-      <VideosSection categoryId={categoryId} />
+      <div className="flex flex-col gap-y-6 pt-2.5">
+        <div className="px-4">
+          <h1 className="text-2xl font-bold">Channel content</h1>
+          <p className="text-xs text-muted-foreground">
+            Manage your channel content and videos
+          </p>
+        </div>
+        <VideosSection categoryId={categoryId} />
+      </div>
     </>
   );
 };
