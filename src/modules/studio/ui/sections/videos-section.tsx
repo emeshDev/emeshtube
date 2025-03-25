634ef2046d3fdf42f05992ab29012b2a1d841033
afdhali <afdhali.apreto@gmail.com>
2025-03-25 13:14:11 +0700

    infinite scrolling

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,14 +1,27 @@
 "use client";
 
+import { InfiniteScroll } from "@/components/infinite-scroll";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 type Props = {
   categoryId?: string | undefined;
 };
 
 export const VideosSection = ({ categoryId }: Props) => {
-  const [data] = trpc.studio.infiniteVideos.useSuspenseInfiniteQuery(
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <VideosSectionSuspense categoryId={categoryId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const VideosSectionSuspense = ({ categoryId }: Props) => {
+  const [data, query] = trpc.studio.infiniteVideos.useSuspenseInfiniteQuery(
     {
       limit: DEFAULT_LIMIT,
       categoryId,
@@ -18,5 +31,15 @@ export const VideosSection = ({ categoryId }: Props) => {
     }
   );
 
-  return <>{JSON.stringify(data)}</>;
+  return (
+    <>
+      {JSON.stringify(data)}
+      <InfiniteScroll
+        isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </>
+  );
 };
